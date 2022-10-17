a9f5741b416db147c18a61b017ef5ce539ec728d
andihasan97 <andihasanelfalakiy@gmail.com>
2022-10-17 09:39:56 +0700

    tablayout dg viewpager2

---
+++ app/src/main/java/com/andihasan7/mytablayout2/SectionPagerAdapter.kt
@@ -0,0 +1,21 @@
+package com.andihasan7.mytablayout2
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SectionPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = HomeFragment()
+            1 -> fragment = ProfileFragment()
+        }
+        return fragment as Fragment
+    }
+
+}
